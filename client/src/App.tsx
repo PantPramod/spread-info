dbf19750555b630577730c04d118f87291d68407
Pramod Pant <pramodpant100@gmail.com>
2023-08-21 15:20:11 +0530

    not found removed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom"
 import Home from './pages'
 import Dashboard from "./pages/Dashboard"
-import NotFound from "./pages/NotFound"
+// import NotFound from "./pages/NotFound"
 import CardPage from "./pages/CardPage"
 const App = () => {
   return (
@@ -9,7 +9,7 @@ const App = () => {
       <Route path="/" element={<Home />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/dashboard/:id" element={<CardPage/>}/>
-      <Route path="*" element={<NotFound />} />
+     
     </Routes>
   )
 }
